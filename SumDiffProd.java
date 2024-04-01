61b6a59a52d265d5712eedef4116180689cfc62b
Ammita Ganddhi <ammita.ganddhi@gmail.com>
2024-04-01 19:51:26 +0530

    java program

--- SumDiffProd.java
+++ SumDiffProd.java
@@ -1,25 +1,56 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class SumDiffProd {
     public static void main(String[] args) {
-        Float num1, num2, sum, dif, prod, avg, max, min;
+
+        Float num1=0.0f, num2 = 0.0f, sum, dif, prod, avg, max, min;
         Scanner s = new Scanner(System.in);
         System.out.print("Enter first number: ");
-        num1 = (float) s.nextDouble();
+        try{
+        num1 = s.nextFloat();
+        } catch (Exception e){
+           // System.out.println("It seems you are trying to enter some different data, so provide again");
+         String b1 = s.next();
+         if(b1.contains("/")){
+            String n = b1.split("/")[0];
+             String d = b1.split("/")[1];
+             num1 = Float.parseFloat(n) / Float.parseFloat(d);
+         }else {
+             System.out.println("It seems you are trying to enter some different data");
+             return;
+         }
+        }
         System.out.print("Enter second number: ");
-        num2 = (float) s.nextDouble();
+       // num2 = (float) s.nextDouble();
+        try{
+            num2 = s.nextFloat();
+        } catch (Exception e){
+           // System.out.println("It seems you are trying to enter some different data, so provide again");
+            String b1 = s.next();
+            if(b1.contains("/")){
+                String n = b1.split("/")[0];
+                String d = b1.split("/")[1];
+                num2 = Float.parseFloat(n) / Float.parseFloat(d);
+            }else {
+                System.out.println("It seems you are trying to enter some different data");
+                return;
+            }
+        }
+
+        DecimalFormat df = new DecimalFormat("0.000 "); //DecimalFormat object df, numbers will be formatted to display exactly three decimal places
 
         sum = num1 + num2;
-        System.out.println("Sum of the two numbers are: " + sum);
+        System.out.println("Sum of the two numbers are: " + df.format(sum));
 
         dif = num1 - num2;
-        System.out.println("Difference of the two numbers are: " + dif);
+        System.out.println("Difference of the two numbers are: " + df.format(dif));
 
         prod = num1 * num2;
-        System.out.println("Product of the two numbers are: " + prod);
+        System.out.println("Product of the two numbers are: " + df.format(prod));
 
         avg = (num1 + num2) / 2;
-        System.out.println("Average of the two numbers are: " + avg);
+        System.out.println("Average of the two numbers are: " + df.format(avg));
 
        /* Maximum number logic using if-else condition
         if(num1 > num2){
@@ -35,7 +66,7 @@ public class SumDiffProd {
 
         min = num1 < num2 ? num1 : num2;
         System.out.println("Minimum number of the two is: " + min);
-        
+
     }
 
 }
