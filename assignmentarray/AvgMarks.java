01748326372efbd0da3828462e394f3f25a6a942
ammita <ammita.ganddhi@gmail.com>
2024-04-30 23:19:44 +0530

    Over all avg of class prog

--- assignmentarray/AvgMarks.java
+++ assignmentarray/AvgMarks.java
@@ -7,47 +7,48 @@ public class AvgMarks {
         Scanner s = new Scanner(System.in);  // creates a new Scanner object named s that reads input from keyboard
 
         System.out.print("Enter the number of students in a class: ");
-        int numstu = s.nextInt();
+        int numstu = s.nextInt();   // parse the input as number of students from console
 
         System.out.print("Enter the number of subjects: ");
-        int numsub = s.nextInt();
+        int numsub = s.nextInt();     // parse the input as number of subjects from console
 
-        int[][] mark = new int[numstu][numsub]; //create array to store subject-wise marks of each student
-        for(int i = 0; i < numstu; i++)
+        int[][] mark = new int[numstu][numsub]; //creates a 2d array called mark to store marks of each student and each subject
+        for(int i = 0; i < numstu; i++)         // for loop that iterates through each student
                 {
                     System.out.println("Let's enter subject-wise marks for student"+ (i+1)+":");
-                    for(int j=0; j< numsub; j++)
+                    for(int j=0; j< numsub; j++)    // for loop that iterates through each subject of current student
                         {
-                            System.out.print("Subject"+ (j + 1) + ": ");
+                            System.out.print("Subject"+ (j + 1) + ": "); // Enter marks of subject1,2,3 and so on...
 
-                            mark[i][j] = s.nextInt();
+                            mark[i][j] = s.nextInt();               // reads integer input and stores it in an array called 'mark'
                          }
 
                 }
+                /* logic to calculate average marks of each student and display the same */
 
-      for(int i=0; i<numstu; i++){
-          int sum=0;
-          for(int j=0; j<numsub; j++){
-              sum+= mark[i][j];
+      for(int i=0; i<numstu; i++){      //for loop that iterates through each student to calculate and display their average marks.
+          int sum=0;                    //variable to store total marks of current student
+          for(int j=0; j<numsub; j++){      // for loop that iterates through each subject for the current student
+              sum+= mark[i][j];         // add marks of current subject and stores it in sum variable
           }
-         double avg = (double) sum / numsub;
-          System.out.println("The Average marks of Student"+ (i+1)+":" + avg);
+         double avg = (double) sum / numsub;        //calculates the average marks of current student by dividing the total marks (sum) by the number of subjects
+          System.out.println("The Average marks of Student"+ (i+1)+":" + avg);   //prints the average marks of the current student to the console.
       }
-      double classavg = calclassavg(mark);
+      double classavg = calclassavg(mark);  // method calling for calculating the over all average marks of class and storing it in variable 'classavg'
         System.out.println("The over all average marks of the class is:" + classavg );
-        s.close();
+        s.close();                      // closes the Scanner object to release the system resources
     }
-private static double calclassavg(int[][] mark){
-        int totalsumofmarks = 0;
-        int totalsubs = mark[0].length;
-        for( int[] stumarks: mark){
-            int sum = 0;
-            for( int mak: stumarks){
-                sum += mak;
+private static double calclassavg(int[][] mark){        //defines a private static method named 'calclassavg' that takes a 2D array of integers (mark) as input parameter and returns a double.
+        int totalsumofmarks = 0;                        //initializes a variable 'totalsumofmarks' to store the total sum of all the marks of all the students
+        int totalsubs = mark[0].length;                 //initializes a variable 'totalsubs' to store the total number of all the subjects. all students have same no. of subjects
+        for( int[] stumarks: mark){                     // for each student's mark in the 'mark' array
+            int sum = 0;                                // initializes variable 'sum' to store the total marks of current student
+            for( int mak: stumarks){                    // for each mark of the current student.
+                sum += mak;                             // adds mark to sum variable
             }
-            totalsumofmarks += sum;
+            totalsumofmarks += sum;                     // adds total marks of current student to the totalsumofmarks variable.
         }
-        return (double) totalsumofmarks / (mark.length * totalsubs);
+        return (double) totalsumofmarks / (mark.length * totalsubs);        // calculates and returns the overall average marks of the class
 }
 }
 
