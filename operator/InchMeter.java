e95c9effb9bd38c04b205763640b8fe3983626af
ammita <ammita.ganddhi@gmail.com>
2024-05-09 19:38:42 +0530

    Over all avg of class prog

--- operator/InchMeter.java
+++ operator/InchMeter.java
@@ -24,3 +24,16 @@ public class InchMeter {
         }
     }
 }
+
+/*
+"C:\Program Files\Java\jdk-11\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2024.1\lib\idea_rt.jar=59833:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2024.1\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\ammit\IdeaProjects\Assignments\out\production\Assignments operator.InchMeter
+Enter the inches: weqwe
+Invalid input. Re-enter the correct input for inch: 1wq
+Invalid input. Re-enter the correct input for inch: qww.3
+Invalid input. Re-enter the correct input for inch: 333.55%
+Invalid input. Re-enter the correct input for inch: #$%
+Invalid input. Re-enter the correct input for inch: -1000.467
+Inches to meter conversion is: -25.41
+
+Process finished with exit code 0
+* */
