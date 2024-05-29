8cc9559472d4ea3930510f300f1ab5ec67a8f2c4
ammita <ammita.ganddhi@gmail.com>
2024-05-29 14:34:41 +0530

    Infinite loop Examples

---
+++ codingAssignments/InfiniteLoopExample.java
@@ -0,0 +1,28 @@
+/* Infinite loop can be written using for loop, While loop and do-while loop */
+
+
+/*for loop: The below for loop will execute infinite as the condition will always be true, since the variable is decremented */
+package codingAssignments;
+
+public class InfiniteLoopExample {
+    public static void main(String[] args) {
+
+        int count = 5, num1=5, i;
+
+        for (i = 0; i < count; --i) {
+            System.out.println("i: " + i);
+        }
+
+
+        while (true) {       // This while loop will execute infinitely as the condition will always be true
+            System.out.println("Infinite While Loop");
+        }
+
+
+        do {                //This do-while loop will execute infinitely as the condition is already false initially
+            System.out.println("Infinite Do-while loop");
+        }while (num1 <=5)
+    }
+}
+
+
