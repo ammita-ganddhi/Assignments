a8b16a77959a7d774002aa51358cc767a2d1d24f
ammita <ammita.ganddhi@gmail.com>
2024-06-05 18:01:14 +0530

    Infinite loop Examples

---
+++ codingAssignments/FindOutput.java
@@ -0,0 +1,15 @@
+package codingAssignments;
+
+public class FindOutput {
+    public static void main(String[] args) {
+        for(;;){
+            System.out.println("HELLO");
+        }
+    }
+    //       int count = 5, i;
+//
+//        for (i = 0; i < count; --i) {
+//            System.out.println("i: " + i);
+//        }
+
+}
--- codingAssignments/InfiniteLoopExample.java
+++ codingAssignments/InfiniteLoopExample.java
@@ -7,21 +7,21 @@ package codingAssignments;
 public class InfiniteLoopExample {
     public static void main(String[] args) {
 
-        int count = 5, num1=5, i;
+        //       int count = 5, num1=5, i;
+//
+//        for (i = 0; i < count; --i) {
+//            System.out.println("i: " + i);
+//        }
+//
+//
+//        while (true) {       // This while loop will execute infinitely as the condition will always be true
+//            System.out.println("Infinite While Loop");
+//        }
 
-        for (i = 0; i < count; --i) {
-            System.out.println("i: " + i);
-        }
-
-
-        while (true) {       // This while loop will execute infinitely as the condition will always be true
-            System.out.println("Infinite While Loop");
-        }
-
-
-        do {                //This do-while loop will execute infinitely as the condition is already false initially
-            System.out.println("Infinite Do-while loop");
-        }while (num1 <=5);
+//
+//        do {                //This do-while loop will execute infinitely as the condition is already false initially
+//            System.out.println("Infinite Do-while loop");
+//        }while (num1 <=5);
     }
 }
 
