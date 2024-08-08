da68594430624f4cd315905e0b109970c15b1ea2
ammita <ammita.ganddhi@gmail.com>
2024-08-08 18:37:14 +0530

    Prime or NonPrime

--- Prime.java
+++ Prime.java
@@ -2,23 +2,46 @@ import java.util.Scanner;
 
 public class Prime {
     public static void main(String[] args) {
-        int i, num, flag = 0;
-        System.out.println("Enter the number");
-        Scanner s = new Scanner(System.in);
-        num = s.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
-        for (i = 2; i < num; i++) {
-            if (num % i == 0) {
-                flag = 0;
-                break;
-            } else {
-                flag = 1;
+        // Input an integer from the user
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+
+        // Edge cases: numbers less than 2 are not prime
+        if (number < 2) {
+            System.out.println(number + " is not Prime.");
+        } else {
+            // Check if the number is prime
+            boolean isPrime = true;
+            for (int i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    isPrime = false;
+                    break;
+                }
             }
-            if (flag == 1) {
-                System.out.println("Prime Number");
-            } else{
-                System.out.println("Not a Prime Number");
+
+            // Output whether the number is prime or not
+            if (isPrime) {
+                System.out.println(number + " is Prime.");
+            } else {
+                System.out.println(number + " is not Prime.");
             }
         }
     }
 }
+
+/*
+"C:\Program Files\Java\jdk-11\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2024.1\lib\idea_rt.jar=54511:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2024.1\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\ammit\IdeaProjects\Assignments\out\production\Assignments Prime
+Enter an integer: 10
+10 is not Prime.
+
+Process finished with exit code 0
+
+"C:\Program Files\Java\jdk-11\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2024.1\lib\idea_rt.jar=54520:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2024.1\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\ammit\IdeaProjects\Assignments\out\production\Assignments Prime
+Enter an integer: 17
+17 is Prime.
+
+Process finished with exit code 0
+
+*/
